c627084597df2ce155cb8227c62a738035ba2ed9
Evandro Barbosa <vandomarinho.vm@gmail.com>
2019-08-01 09:55:56 -0300

    Criando o app atm_consultoria

---
+++ atm_consultoria/lib/AtmConsultoria.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AtmConsultoria extends StatefulWidget {
+  @override
+  _AtmConsultoriaState createState() => _AtmConsultoriaState();
+}
+
+class _AtmConsultoriaState extends State<AtmConsultoria> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ATM Consultoria"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ atm_consultoria/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:atm_consultoria/AtmConsultoria.dart';
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    home: AtmConsultoria(),
+  ));
+}
