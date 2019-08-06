38a9bd6a242755253926ed587c33cb9361102013
Evandro Barbosa <vandomarinho.vm@gmail.com>
2019-08-06 15:04:18 -0300

    trabalhando alertDialog

--- lista/lib/Home.dart
+++ lista/lib/Home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
     for (var i = 0; i <= 10; i++) {
       Map<String, dynamic> item = Map();
       item["titulo"] = "Titulo ${i} Valor do dolar";
-      item["descricao"] = "Descrição ${1} o Dolar hoje esta a R\$ 3,95 com queda de 0.89%";
+      item["descricao"] = "Descrição ${i} o Dolar hoje esta a R\$ 3,95 com queda de 0.89%";
       _itens.add(item);
     }
   }
@@ -35,6 +35,51 @@ class _HomeState extends State<Home> {
             print("Item: ${_itens[index]["titulo"]}");
 
             return ListTile(
+              onTap: (){
+                // print("Clique selecionado");
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(_itens[index]["titulo"]),
+                      titlePadding: EdgeInsets.all(20),
+                      titleTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.red
+                      ),
+                      content: Text(_itens[index]["descricao"]),
+                      actions: <Widget>[
+                        FlatButton(
+                          color: Colors.green,
+                          child: Text(
+                            "Sim",
+                            style: TextStyle(
+                              color: Colors.white
+                            ),
+                          ),
+                          onPressed: (){
+                            print("Selecionado sim");
+                            Navigator.pop(context);
+                          },
+                        ),
+                        FlatButton(
+                          color: Colors.red,
+                          child: Text(
+                            "Não",
+                            style: TextStyle(
+                              color: Colors.white
+                            ),
+                          ),
+                          onPressed: (){
+                            print("Selecionado não");
+                            Navigator.pop(context);
+                          },
+                        )
+                      ],
+                    );
+                  }
+                );
+              },
               title: Text(_itens[index]["titulo"]),
               subtitle: Text(_itens[index]["descricao"]),
             );
